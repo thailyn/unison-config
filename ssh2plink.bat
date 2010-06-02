7a7895e7a89ced524ab6741bfc76bad843681641
Charles Macanka <cmacanka@gmail.com>
2010-06-01 20:39:56 -0400

    Updating ssh2plink.bat based on home pc version.
    Further revising the ssh2plink.bat file based on the version from my home pc,
    which in turn was based on the version from Miyuki.  This version encapsulates
    some of the argument logic in helper functions and adds a generous amount of
    comments.  It also adds a [settings] section to the ini file, whcih can be used
    in bat files calling Unison to customize the arguments passed to Unison.

--- ssh2plink.bat
+++ ssh2plink.bat
@@ -8,58 +8,85 @@ rem	-p nnn (replaced by P); -e none (no equivalent)
 
 setlocal
 
-rem set INIFILE="C:\Users\Charles\.unison\settings.ini"
 set INIFILE="%~dp0settings.ini"
-call:getvalue %INIFILE% "plinkexe" "" PLINKEXE
+call:getvalue %INIFILE% "fullplinkexe" "" PLINKEXE
 call:getvalue %INIFILE% "username" "" USERNAME
 call:getvalue %INIFILE% "servername" "" SERVERNAME
 
+rem  To test reading from the ini file:
 rem echo plinkexe: %PLINKEXE%
 rem echo username: %USERNAME%
 rem echo servername: %SERVERNAME%
 rem goto:eof
 
-rem set ARGLIST=-ssh
+rem  Initialize the argument list based on the ini file contents.
 set ARGLIST=%SERVERNAME% -l %USERNAME%
 
+rem  On Windows XP this file can be run with the path to itself, and if there 
+rem are spaces in the path it will take the place of multiple arguments.  The
+rem first parameter we care about starts with the server name, so throw out 
+rem arguments until we get to that point.
+:filterpath
+if "%1" == "%SERVERNAME%" goto nextarg
+   shift
+   goto filterpath
+
+rem  Start of loop for parsing arguments.
 :nextarg
 
-if "x%1" == "x" goto doneargs
+rem  Exit out of the loop if we are out of arguments.
+if "[%1]" == "[]" goto doneargs
 
-if NOT "%1" == "-p" goto checkesc
-	rem plink uses upper case P to flag port number
-	set ARGLIST=%ARGLIST% -P %2
-	shift
-	shift
-	goto nextarg
+rem  Handle arguments that require nontrivial consideration.
+if "%1" == "-p" goto setport
+if "%1" == "-pw" goto setpassword
+if "%1" == "%SERVERNAME%" goto setserver
+if "%1" == "-e" goto droparge
 
-:checkesc
-
-if "%1" == "%SERVERNAME%" goto skiparg
-
-if NOT "%1" == "-e" goto useit
-	rem unison likes to include "-e none" which plink does not support.
-	shift
-	shift
-	goto nextarg
-
+rem  Absorb other arguments (trivially add them).
 :useit
+        set ARGLIST=%ARGLIST% %1
+        shift
 
-	set ARGLIST=%ARGLIST%  %1
-	shift
-
+rem  Loop to the next argument.
 goto nextarg
-
-:skiparg
-	shift
-	goto nextarg
 
 endlocal
 
 :doneargs
-%PLINKEXE%  %ARGLIST%
+rem echo %PLINKEXE% %ARGLIST%
+"%PLINKEXE%" %ARGLIST%
 
 goto:eof
+
+rem ---------------------------------------------------
+rem ---------------------------------------------------
+rem Helper Functions:
+
+:setport
+rem plink uses upper case P to flag port number
+set ARGLIST=%ARGLIST% -P %2
+shift
+shift
+goto nextarg
+
+:setserver
+rem we actually don't need the server from the command line.  remove it
+shift
+goto nextarg
+
+:setpassword
+set ARGLIST=%ARGLIST% -pw %2
+shift
+shift
+goto nextarg
+
+:droparge
+rem unison likes to include "-e none" which plink does not support.
+shift
+shift
+goto nextarg
+
 
 :getvalue
 rem %1 = name of ini file to search in.
@@ -69,3 +96,4 @@ rem %4 = variable to place search result
 FOR /F "eol=; eol=[ tokens=1,2,3* delims==" %%i in ('findstr /b /l /i %~2= %1') DO set %~4=%%~j
 rem replace last part with %%~j to take surrounding " out of j
 goto:eof
+
