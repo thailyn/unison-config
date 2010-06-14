7a913eb17f1da013e41149d1d45f120207c15c07
Charles Macanka <cmacanka@gmail.com>
2010-06-14 11:41:31 -0400

    Updated bat script to use (updated) settings.ini.
    The unison-update-logon.bat file now reads settings from the settings.ini file,
    including which version of unison to use and other properties to configure
    Unison.  The settings.ini file was updated to point to the correct version of
    plink and use the newest version of Unison.  It also has the correct profile
    specified and the proper output level.

--- unison-update-logon.bat
+++ unison-update-logon.bat
@@ -1,9 +1,28 @@
 @echo off
-echo This script will sync against the rsync partner on atheneum-caelestis.dynalias.net.  Set up Pageant before continuing.
+setlocal
+
+rem cd c:/cygwin/bin
+set INIFILE="%~dp0settings.ini"
+call:getvalue %INIFILE% "servername" "" SERVERNAME
+call:getvalue %INIFILE% "unisonpath" "" UNISONPATH
+call:getvalue %INIFILE% "outputlevel" "" OUTPUTLEVEL
+call:getvalue %INIFILE% "fastcheck" "" FASTCHECKVAL
+call:getvalue %INIFILE% "profilefile" "" PROFILEFILE
+
+echo This script will sync against the root on %SERVERNAME%.  Set up Pageant before continuing.
+pause
+%UNISONPATH% %PROFILEFILE% -fastcheck %FASTCHECKVAL% %OUTPUTLEVEL%
 pause
 
-rem set /P password=Password:
-rem echo passwrd:%password%
-rem c:/cygwin/bin/unison-2.27.exe driver-logon -fastcheck yes -sshargs "-pw %password%"
-c:/cygwin/bin/unison-2.27.exe driver-logon -fastcheck yes
-pause
+endlocal
+goto:eof
+
+
+:getvalue
+rem %1 = name of ini file to search in.
+rem %2 = search term to look for
+rem %3 = group name (not currently used)
+rem %4 = variable to place search result
+FOR /F "eol=; eol=[ tokens=1,2,3* delims==" %%i in ('findstr /b /l /i %~2= %1') DO set %~4=%%~j
+rem replace last part with %%~j to take surrounding " out of j
+goto:eof
